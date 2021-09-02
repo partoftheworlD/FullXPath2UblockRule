8174e0e9af9cb059a2c64b093cb43118563907b7
partoftheworlD <partoftheworldcs@gmail.com>
2021-09-02 13:42:27 +0300

    'upd'

--- FullXPath2UblockRule.sh
+++ FullXPath2UblockRule.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/bash
 url=$1
 xpath=$2
-echo "$url ##$xpath" | sed -e 's/\/\//\//g;s/\/html/##html/;s/\// \> /g;s/\[/:nth-of-type\(/g;s/\]/\)/g'
+echo "$url##$xpath" | sed -e 's/\/\//\//g;s/\/html/##html/;s/\// \> /g;s/\[/:nth-of-type\(/g;s/\]/\)/g'
