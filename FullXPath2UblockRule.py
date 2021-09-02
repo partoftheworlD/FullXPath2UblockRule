66593ac181db38b0eb8b443c04c9e6a5f1bbc007
partoftheworlD <partoftheworldcs@gmail.com>
2021-09-02 13:41:22 +0300

    fixed weird xpath case and added a non-python alternative

--- FullXPath2UblockRule.py
+++ FullXPath2UblockRule.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
 import sys
 
+
 def generate_rule(site, xpath):
     temp_string = ''
 
     temp_string += site
 
+    xpath = xpath.replace('//', '/')
     xpath = xpath.replace('/html', '##html')
     xpath = xpath.replace('/', ' > ')
     xpath = xpath.replace('[', ":nth-of-type(")
@@ -14,11 +16,13 @@ def generate_rule(site, xpath):
     temp_string += xpath
     print(temp_string)
 
+
 def main():
     if len(sys.argv) != 3:
         print("Example: FullXPath2UblockRule.py site fullxpath")
     else:
         generate_rule(sys.argv[1], sys.argv[2])
+
 
 if __name__ == '__main__':
     main()
