2ae3b22ca93f0a6aead30e2ffe7004b5847c271e
partoftheworlD <partoftheworldcs@gmail.com>
2021-09-05 11:06:49 +0300

    Update FullXPath2UblockRule.sh

--- FullXPath2UblockRule.sh
+++ FullXPath2UblockRule.sh
@@ -4,7 +4,7 @@ xpath=$2
 
 if [[ -n "$url" &&  -n "$xpath" ]]
 then
-        echo "$url##$xpath" | sed -e 's/\/\//\//g;s/\/html/##html/;s/\// \> /g;s/\[/:nth-of-type\(/g;s/\]/\)/g'
+        echo "$url$xpath" | sed -e 's/\/\//\//g;s/\/html/##html/;s/\// \> /g;s/\[/:nth-of-type\(/g;s/\]/\)/g'
 else
         echo "Usage: $0 <url> <full xpath>"
 fi
